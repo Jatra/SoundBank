54788be65a3380eae13de6557e9f6a5fa9024bc7
Tim Russell <trussell@zipcar.co.uk>
2016-01-03 17:07:20 +0000

    Documentation of the SoundBank API

--- soundbanklib/src/main/java/uk/co/jatra/soundbanklib/API.java
+++ soundbanklib/src/main/java/uk/co/jatra/soundbanklib/API.java
@@ -1,22 +1,78 @@
 package uk.co.jatra.soundbanklib;
 
 /**
- * Created by tim on 03/01/2016.
+ * THe interface of the Soundbank.
  */
 public interface API {
-    public void preload(String[] ids);
-    public void play(String id);
-    public void record(String id);
-    public boolean available(String id);
-    public void stopRecording(boolean save);
-    public void stopPlaying();
+    /**
+     * Play the specified audio.
+     * @param id of the wanted sound in the bank
+     */
+    void play(String id);
+
+    /**
+     * Record the specified audio.
+     * @param id of the sound to be saved
+     */
+    void record(String id);
+
+    /**
+     * Test if sound name is available.
+     * @param id wanted sound.
+     * @return true if the soundbank has the sound
+     */
+    boolean available(String id);
+
+    /**
+     * Finish the current recording.
+     * @param save whether to save the recording into the bank.
+     */
+    void stopRecording(boolean save);
+
+    /**
+     * Finish playing the sound in progress.
+     */
+    void stopPlaying();
+
+    /**
+     * Determine if the soundbank is currently recording a sound.
+     * @return true if recording
+     */
     boolean isRecording();
+
+    /**
+     * Determine if the soundbank is currently playing a sound.
+     * @return true if playing
+     */
     boolean isPlaying();
+
+    /**
+     * Set a listener for started/completed of playing/recording.
+     * @param stateListener an instance implementing the callback methods
+     */
     void setStateListener(StateListener stateListener);
+
+    /**
+     * Set the soundbank play volume
+     * @param leftVolume left volume 0 to 1.0
+     * @param rightVolume right volume 0 to 1.0
+     */
     void setVolume(float leftVolume, float rightVolume);
 
-    public interface StateListener {
+    /**
+     * Instances implementing StateListener and set via setStateListener
+     * will be notified of audio start/completion.
+     */
+    interface StateListener {
+        /**
+         * The named audio has started.
+         * @param id the id of the started audio.
+         */
         void started(String id);
+
+        /**
+         * The soundbank has completed its current audio.
+         */
         void completed();
     }
 }
